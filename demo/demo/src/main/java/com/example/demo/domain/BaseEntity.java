d81d74084ecc1c05e902ea21ccd62f3e36101d3d
jotbab <als97@kakao.com>
2023-01-16 17:02:33 +0900

    spring boot 설정

---
+++ demo/demo/src/main/java/com/example/demo/domain/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+/*
+ * @MappedSuperclass
+ * 공통된 매핑 정보가 필요한 경우에 사용
+ * 추상화(abstract)사용 권장
+ */
+@MappedSuperclass
+@EntityListeners(value = { AuditingEntityListener.class})
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "regdate", updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(name = "moddate", updatable = false)
+    private LocalDateTime modDate;
+
+}
---
+++ demo/demo/src/main/java/com/example/demo/domain/Board.java
@@ -0,0 +1,51 @@
+package com.example.demo.domain;
+
+import lombok.*;
+
+
+import javax.persistence.*;
+
+/*
+* === board Table ===
+* create table board (
+* bid bigint primary key auto increment,
+* title varchar(100) not null,
+* content varchar(1000) not null,
+* writer varchar(30) not null,
+* regdate datetime default now(),
+* moddate datetime default now
+* );
+*
+ */
+
+
+
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bid;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 1000, nullable = false)
+    private String content;
+
+    @Column(length = 30, nullable = false)
+    private String writer;
+
+    //update문으로 바뀔 여지가 있는것들
+    public void change(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
